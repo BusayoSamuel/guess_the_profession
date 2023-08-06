8ef9ec553f3a7619a9f4aaf49853d82a415130d9
BusayoSamuel <busayosalisu@gmail.com>
2023-08-06 08:01:24 +0100

    progress on gameplay screen

--- guess_the_profession/lib/screens/gameplay.dart
+++ guess_the_profession/lib/screens/gameplay.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 
 class Gameplay extends StatefulWidget {
   const Gameplay({super.key, required this.title});
@@ -10,64 +11,112 @@ class Gameplay extends StatefulWidget {
 }
 
 class _GameplayState extends State<Gameplay> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFFDAFFFB),
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor: const Color(0xFF001C30),
+        title: Text(
+          widget.title,
+          style: const TextStyle(color: Color(0xFFDAFFFB)),
+        ),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 5),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            Image.asset("assets/images/Icon-192.png"),
-            const Text("Answer"),
-            const Text("Options"),
-            TextButton(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.grey),
-                shape: MaterialStateProperty.all(
-                  RoundedRectangleBorder(
-                    side: const BorderSide(
-                      color: Colors.black,
-                      width: 1.0,
-                    ),
-                    borderRadius: BorderRadius.circular(5.0),
-                  ),
-                ),
-                minimumSize: MaterialStateProperty.all(const Size(50.0, 50.0)),
-              ),
-              onPressed: () {},
-              child: const Text("A"),
+            Image.asset(
+              "assets/images/doctor/stethoscope.jpg",
+              height: 250,
             ),
+            const Divider(
+              color: Colors.black,
+              thickness: 1,
+            ),
+            answerBoxes(context, 6),
+            const Divider(
+              color: Colors.black,
+              thickness: 1,
+            ),
+            GridView(
+                shrinkWrap: true,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 6,
+                ),
+                children: letterOptions(context, letters: [
+                  "T",
+                  "B",
+                  "C",
+                  "D",
+                  "O",
+                  "F",
+                  "O",
+                  "I",
+                  "J",
+                  "C",
+                  "L",
+                  "R",
+                ])),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
-    ;
   }
+}
+
+List<Widget> letterOptions(BuildContext context,
+    {required List<String> letters}) {
+  List<Widget> children = [];
+  final w = (MediaQuery.of(context).size.width - 4 * (12 - 1)) / 12;
+
+  for (final letter in letters) {
+    children.add(letterButton(letter: letter));
+  }
+  return children;
+}
+
+Widget letterButton({required String letter, double size = 30}) {
+  return TextButton(
+    style: ButtonStyle(
+      backgroundColor: MaterialStateProperty.all(Color(0xFF001C30)),
+      shape: MaterialStateProperty.all(
+        RoundedRectangleBorder(
+          side: const BorderSide(
+            color: Colors.white,
+            width: 1.0,
+          ),
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+      ),
+      fixedSize: MaterialStateProperty.all(Size(size, size)),
+      padding: MaterialStateProperty.all(EdgeInsets.zero),
+    ),
+    onPressed: () {},
+    child: AutoSizeText(
+      letter,
+      style: const TextStyle(fontSize: 45, color: Color(0xFFDAFFFB)),
+    ),
+  );
+}
+
+Widget answerBoxes(BuildContext context, answerLength) {
+  final size = (MediaQuery.of(context).size.width - 14 * (6 - 1)) / 6;
+
+  return Wrap(
+    runSpacing: 4,
+    spacing: 4,
+    alignment: WrapAlignment.center,
+    children: List.generate(6, (index) {
+      return Container(
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+          color: Color(0xFF001C30),
+        ),
+        width: size,
+        height: size,
+      );
+    }),
+  );
 }
